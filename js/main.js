43e1c8a49e9f50883277782c2967ee85f82dc99a
yersultanzada <yersultan.zada@gmail.com>
2018-08-18 17:38:52 +0600

    some

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
-
+    $('#review-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
 
 });
