09a146025f2d229025f76ac56f1cebb93bf5e467
yersultanzada <yersultan.zada@gmail.com>
2018-08-25 00:11:34 +0600

    with feedback

--- js/main.js
+++ js/main.js
@@ -35,4 +35,12 @@ $(document).ready(function () {
         }, false);
     })();
 
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+
 });
