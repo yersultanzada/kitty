71dbac2c304bc65d7a5208a7812fb813e1266a0b
yersultanzada <yersultan.zada@gmail.com>
2018-08-24 18:50:22 +0600

    ...

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#review-carousel').owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
+        nav:false,
         responsive:{
             0:{
                 items:1
@@ -14,6 +14,25 @@ $(document).ready(function () {
                 items:5
             }
         }
-    })
+    });
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
 
 });
